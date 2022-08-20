5b8ea045e9ce5a69187b3b2c132c6a8ee0b40de6
BRUMKEL25 <111489636+BRUMKEL25@users.noreply.github.com>
2022-08-20 07:10:06 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['085218734569'] //ur owner number
+global.ownername = "Brumkel😈Maker" //ur owner name
+global.ytname = "YT: Brumkel25" //ur yt chanel name
+global.socialm = "GitHub: No" //ur github or insta name
+global.location = "🛡️⚔️🛡️" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.owner = ['https://wa.me/6285218734569']
+global.ownertag = '6285218734569' //ur tag number
+global.botname = 'Slipper Botz' //ur bot name
+global.linkz = "https://youtu.be/" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtu.be/" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.reactmoji = "😈" //ur menu react emoji
+global.themeemoji = "🥶" //ur theme emoji
+global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n⚔️🛡️⚔️ " //ur sticker watermark packname
+global.author = "🤖BOT🤖" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -122,7 +122,7 @@ global.mess = {
 }
     global.limitawal = {
     premium: "Infinity",
-    free: 12,
+    free: 10,
     monayawal: 1000
 }
    global.rpg = {
